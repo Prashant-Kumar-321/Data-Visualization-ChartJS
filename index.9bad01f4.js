76ac8b0e81b8cfd237a794979392d8db44c61f75
Prashant-kumar-321 <pk6176838@gmail.com>
2025-03-12 22:28:17 +0530

    Updates

---
+++ index.9bad01f4.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("canvas-container"),t=document.getElementById("price-history");window.addEventListener("resize",()=>{let n=e.clientWidth,d=e.clientHeight;t.width=n,t.height=d,t.style.width=`${n}px`,t.style.height=`${d}px`})});
+//# sourceMappingURL=index.9bad01f4.js.map
