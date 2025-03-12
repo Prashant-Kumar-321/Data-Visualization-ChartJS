97bbb03a1e4d579f9444a14c26774e9f99546bf8
Prashant-kumar-321 <pk6176838@gmail.com>
2025-03-12 22:30:00 +0530

    Include dist folder in track

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const canvasContainer = document.getElementById('canvas-container');
+    const canvas = document.getElementById('price-history');
+    window.addEventListener('resize', ()=>{
+        const contWidth = canvasContainer.clientWidth;
+        const contHeight = canvasContainer.clientHeight;
+        canvas.width = contWidth;
+        canvas.height = contHeight;
+        canvas.style.width = `${contWidth}px`;
+        canvas.style.height = `${contHeight}px`;
+    });
+});
+
+//# sourceMappingURL=index.816e7b21.js.map
